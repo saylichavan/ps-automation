360bdc57bb07f1d059f216f9bc85fcecf1bd98de
Sayli <chavansayli93@gmail.com>
2019-08-06 21:51:21 -0400

    Infinite scroll

---
+++ test/Infinitescroll.test.js
@@ -0,0 +1,40 @@
+const { describe, it, after, before } = require('mocha');
+const Page = require('../lib/InfiniteScroll');
+
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+process.on('unhandledRejection', () => { });
+
+(async function example() {
+    try {
+        describe.only('Internet Herokuapp Infinite Scrolling automated testing', async function () {
+            this.timeout(5000);
+            let driver, page;
+
+            beforeEach(async () => {
+                page = new Page();
+                driver = page.driver;
+                await page.visit('https://the-internet.herokuapp.com/infinite_scroll');
+            });
+
+            afterEach(async () => {
+                await page.quit();
+            });
+
+            it('Successfully visited the InfiniteScrolling page', async () => {
+                
+                const scroll = await page.InfiniteScroll();
+                expect(scroll).to.be.equals(true);
+                
+            });
+
+        });
+    } catch (ex) {
+        console.log(new Error(ex.message));
+    } finally {
+
+    }
+})();
--- utils/locator.js
+++ utils/locator.js
@@ -6,5 +6,10 @@ module.exports = {
     logouttext: 'Logout',
     tableSelectorId: 'table1',
     sortln: '//*[@id="table1"]/thead/tr/th[1]/span',
+    dropdwn: '//*[@id="dropdown"]',
+    dropdownid:'dropdown',
+    option1:'Option 1',
+    checkbox1:'checkbox 1',
+    divclass='jscroll-inner'
 
 };
