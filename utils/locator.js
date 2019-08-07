2174969d94b8ea92e5cecae4d4823ff61f383d7f
sayli chavan <chavansayli93@gmail.com>
2019-08-07 11:35:13 -0400

    Final Upload without infinite scrolling

--- utils/locator.js
+++ utils/locator.js
@@ -7,9 +7,7 @@ module.exports = {
     tableSelectorId: 'table1',
     sortln: '//*[@id="table1"]/thead/tr/th[1]/span',
     dropdwn: '//*[@id="dropdown"]',
-    dropdownid:'dropdown',
-    option1:'Option 1',
-    checkbox1:'checkbox 1',
-    divclass='jscroll-inner'
-
+    dropdownid: 'dropdown',
+    option1: 'Option 1',
+    checkbox1: 'checkbox 1',
 };
